23dd19b99eba5562161349c8c6c9f418b5a05b7c
jnb2387 <jnb2387@gmail.com>
2016-04-08 15:26:15 +0000

    Loop Practice

---
+++ script.js
@@ -0,0 +1,40 @@
+// #1
+for(var x = 0; x < 501; x+=100){
+    console.log(x);
+}
+
+// #2
+for(var x = 1; x < 65; x*=2){
+    console.log(x);
+}
+
+// #3
+for(var x = 1; x < 4; x++){
+    for(var j = 0; j < 3; j++) {
+       console.log(x);
+    }
+}
+
+// #4
+for(var x = 0; x < 6 ; x++){
+    console.log(x * 2);
+}
+
+// #5
+for(var x = 1; x < 6; x++) {
+    console.log(x * 3);
+}
+
+// #6
+for(var x = 9; x >= 0; x--){
+    console.log(x);
+}
+
+// #7
+for (i = 1; i < 4; i++) {
+   for (lc = 0; lc < 4; lc++) {
+      console.log(lc);
+  }
+}
+   
+
